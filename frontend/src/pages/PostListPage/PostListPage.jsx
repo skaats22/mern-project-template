f30441ccfbb05346f692d357db507aa3c2adef25
Steve Kaats <stevenakaats@gmail.com>
2025-01-22 09:44:27 -0800

    Empty PostListPage initialize array

--- frontend/src/pages/PostListPage/PostListPage.jsx
+++ frontend/src/pages/PostListPage/PostListPage.jsx
@@ -6,28 +6,7 @@ import PostItem from '../../components/PostItem/PostItem';
 export default function PostListPage() {
 
   // If state going to be array, then initialize to []
-  const [posts, setPosts] = useState([
-    {
-      content: 'Hello there from Kate',
-      createdAt: '2025-01-22T08:35:22',
-      _id: '90asdha9',
-      user: {
-        name: 'Kate',
-        email: 'kate@email.com',
-        _id: 'a45fb15',
-      },
-    },
-    {
-      content: 'Hello there from Justin',
-      createdAt: '2025-01-21T06:35:22',
-      _id: '90asdhsdfsda9',
-      user: {
-        name: 'Justin',
-        email: 'justin@email.com',
-        _id: 'a45fb16',
-      },
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     async function fetchPosts() {
